0d840075068041c759817c31d94f78fe1df34c97
ömer sarıtemur <palaomer3056@windowslive.com>
2022-09-12 07:38:32 +0300

    Düzeltmeler

--- Application/Features/Users/Commands/CreateUsers/RegisterUserCommandValidator.cs
+++ Application/Features/Users/Commands/CreateUsers/RegisterUserCommandValidator.cs
@@ -11,24 +11,15 @@ namespace Application.Features.Users.Commands.CreateUsers
     {
         public RegisterUserCommandValidator()
         {
-            RuleFor(x => x.Email).NotEmpty();
-            
+            RuleFor(x => x.Email).NotEmpty();            
             RuleFor(c => c.Password).NotEmpty();
-            RuleFor(c => c.Password).MinimumLength(8);
-            RuleFor(c => c.Password).Must(ContainLDS);
+            RuleFor(c => c.Password).MinimumLength(8);           
             RuleFor(c => c.FirstName).NotEmpty();
             RuleFor(c => c.FirstName).MinimumLength(2);
             RuleFor(c => c.LastName).NotEmpty();
             RuleFor(c => c.LastName).MinimumLength(2);
         }
 
-        private bool ContainLDS(string arg)
-        {
-            return arg is not null
-                && arg.Any(a => char.IsUpper(a))
-                && arg.Any(a => char.IsLower(a))
-                && arg.Any(a => char.IsDigit(a))
-                && arg.Any(a => char.IsSymbol(a) || char.IsPunctuation(a));
-        }
+        
     }
 }
