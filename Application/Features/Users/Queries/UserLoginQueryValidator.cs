ed66716dd6ccd9bc275ef8c8b9f5fefa8882b602
ömer sarıtemur <palaomer3056@windowslive.com>
2022-09-12 07:41:59 +0300

    Düzeltmeler

--- Application/Features/Users/Queries/UserLoginQueryValidator.cs
+++ Application/Features/Users/Queries/UserLoginQueryValidator.cs
@@ -12,19 +12,9 @@ namespace Application.Features.Users.Queries
         public UserLoginQueryValidator()
         {
             RuleFor(c => c.Email).NotEmpty();
-            RuleFor(c => c.Email).EmailAddress();
             RuleFor(c => c.Password).NotEmpty();
             RuleFor(c => c.Password).MinimumLength(8);
-            RuleFor(c => c.Password).Must(ContainLDS);
         }
 
-        private bool ContainLDS(string arg)
-        {
-            return arg is not null
-                && arg.Any(a => char.IsUpper(a))
-                && arg.Any(a => char.IsLower(a))
-                && arg.Any(a => char.IsDigit(a))
-                && arg.Any(a => char.IsSymbol(a) || char.IsPunctuation(a));
-        }
     }
 }
